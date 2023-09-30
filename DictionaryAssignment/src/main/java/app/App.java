0f30d13015527cfcb529525fec2a91bbffc77eda
DuyPhong <duyphong.rev@gmail.com>
2023-09-30 22:54:47 +0700

    raw layout

--- DictionaryAssignment/src/main/java/app/App.java
+++ DictionaryAssignment/src/main/java/app/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("container.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 480);
+        Scene scene = new Scene(fxmlLoader.load(), 760, 480);
         stage.setScene(scene);
         stage.show();
     }
