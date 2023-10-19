5cb481134f90b45cefcb02d5a45c59baa12df259
DzungDz <huudung141434@gmail.com>
2023-10-19 15:26:23 +0700

    test

--- DictionaryAssignment/src/main/java/app/App.java
+++ DictionaryAssignment/src/main/java/app/App.java
@@ -16,6 +16,7 @@ public class App extends Application {
     }
     @Override
     public void start(Stage stage) throws IOException, JavaLayerException {
+        System.out.println("test");
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("container.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 760, 480);
         stage.setScene(scene);
