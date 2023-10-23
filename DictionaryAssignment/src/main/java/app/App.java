2c10a916942f7ca824e2ba347bb59dff4fb252a1
DuyPhongRev <duyphong.rev@gmail.com>
2023-10-23 11:26:20 +0700

    change logo

--- DictionaryAssignment/src/main/java/app/App.java
+++ DictionaryAssignment/src/main/java/app/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("container.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
-        stage.getIcons().add(new Image(App.class.getResourceAsStream("R.png")));
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("logo.png")));
         stage.show();
     }
 }
